8c4a0634ef559bf4c6ab85b830d8b36a9beb30fb
Denis Grukhin <denis.grukhin@t-systems.com>
2021-06-28 20:54:16 +0300

    The simplest REST service is tested and ready to go

--- src/main/java/denpear/formyrestful/controller/MessageController.java
+++ src/main/java/denpear/formyrestful/controller/MessageController.java
@@ -2,10 +2,7 @@ package denpear.formyrestful.controller;
 
 
 import denpear.formyrestful.exceptions.NotFoundException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,24 +10,59 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping ("message")
+@RequestMapping("message")
 public class MessageController {
-    public List <Map<String,String>> messages = new ArrayList<>() {{
-        add (new HashMap<>(){{put("id", "1"); put("text", "First message"); }});
-        add (new HashMap<>(){{put("id", "2"); put("text", "Second message"); }});
-        add (new HashMap<>(){{put("id", "3"); put("text", "Third message"); }});
+    int counter = 4;
+    public List<Map<String, String>> messages = new ArrayList<>() {{
+        add(new HashMap<>() {{
+            put("id", "1");
+            put("text", "First message");
+        }});
+        add(new HashMap<>() {{
+            put("id", "2");
+            put("text", "Second message");
+        }});
+        add(new HashMap<>() {{
+            put("id", "3");
+            put("text", "Third message");
+        }});
     }};
 
     @GetMapping
-    public List<Map<String, String>> list(){
+    public List<Map<String, String>> list() {
         return messages;
     }
 
     @GetMapping("{id}")
-    public Map<String,String> getOne (@PathVariable String id){
+    public Map<String, String> getOne(@PathVariable String id) {
+        return getMessage(id);
+    }
+
+    private Map<String, String> getMessage(@PathVariable String id) {
         return messages.stream()
                 .filter(message -> message.get("id").equals(id))
                 .findFirst()
                 .orElseThrow(NotFoundException::new);
+    }
+    // в консоли используем fetch('/message', { method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({text: 'message 4'})}).then(result => console.log(result))
+    @PostMapping
+    public Map<String, String> create(@RequestBody Map<String, String> message) {
+        message.put("id", String.valueOf(counter++));
+        messages.add(message);
+        return message;
+    }
+    //fetch('/message/4', { method: 'PUT', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({text: 'Fourth message (4)', id: 10 })}).then(result => console.log(result))
+    @PutMapping ("{id}")
+    public Map<String, String> update(@PathVariable String id, @RequestBody Map<String, String> message) {
+        Map<String, String> messageFromDb = getMessage(id);
+        messageFromDb.putAll(message);
+        messageFromDb.put("id", id);
+        return messageFromDb;
+    }
+    //fetch('/message/4', { method: 'DELETE' }).then(result => console.log(result))
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id) {
+        Map<String, String> message = getMessage(id);
+        messages.remove(message);
     }
 }
