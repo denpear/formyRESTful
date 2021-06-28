f7214f057ffba5f644e6e62b5fe11dd71f7c945e
Denis Grukhin <denis.grukhin@t-systems.com>
2021-06-28 19:57:27 +0300

    Return instead index messages, second get URL for id of concrete message

--- src/main/java/denpear/formyrestful/controller/MessageController.java
+++ src/main/java/denpear/formyrestful/controller/MessageController.java
@@ -1,16 +1,36 @@
 package denpear.formyrestful.controller;
 
 
+import denpear.formyrestful.exceptions.NotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping ("message")
 public class MessageController {
-    @GetMapping
+    public List <Map<String,String>> messages = new ArrayList<>() {{
+        add (new HashMap<>(){{put("id", "1"); put("text", "First message"); }});
+        add (new HashMap<>(){{put("id", "2"); put("text", "Second message"); }});
+        add (new HashMap<>(){{put("id", "3"); put("text", "Third message"); }});
+    }};
 
-    public String list(){
-        return "index";
+    @GetMapping
+    public List<Map<String, String>> list(){
+        return messages;
+    }
+
+    @GetMapping("{id}")
+    public Map<String,String> getOne (@PathVariable String id){
+        return messages.stream()
+                .filter(message -> message.get("id").equals(id))
+                .findFirst()
+                .orElseThrow(NotFoundException::new);
     }
 }
